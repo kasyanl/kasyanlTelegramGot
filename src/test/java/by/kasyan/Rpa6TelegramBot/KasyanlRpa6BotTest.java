c61fa31f5020748d5b127fa92e0f2c3d8d947baa
max-java <6666350@gmail.com>
2020-11-25 19:33:20 +0300

    optimized structure

---
+++ src/test/java/by/kasyan/Rpa6TelegramBot/KasyanlRpa6BotTest.java
@@ -0,0 +1,24 @@
+package by.kasyan.Rpa6TelegramBot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KasyanlRpa6BotTest {
+
+    @Test
+    void getBotUsername() {
+    }
+
+    @Test
+    void getBotToken() {
+    }
+
+    @Test
+    void onUpdateReceived() {
+    }
+
+    @Test
+    void sendMesg() {
+    }
+}
