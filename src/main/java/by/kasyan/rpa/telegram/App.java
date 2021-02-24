396a2dfc30327e99402d04b152bf4ef8b22afefe
Kasyanl <1784158@tut.by>
2021-02-24 09:30:08 +0300

    hello

--- src/main/java/by/kasyan/rpa/telegram/App.java
+++ src/main/java/by/kasyan/rpa/telegram/App.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
-public class TelegramBotApplication {
+public class App {
 
 	public static void main(String[] args) {
 		ApiContextInitializer.init();
 
-		SpringApplication.run(TelegramBotApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 }
